485f999708db6d0bec241c232469eb430220ddd1
savaged <savagedavid@gmail.com>
2020-12-21 18:45:47 +0000

    r h and k as optional arguments

--- Circle/Main.c
+++ Circle/Main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <math.h>
 
-// TODO: make these user arguments 
-#define R 5
-#define H 6
-#define K 8
+int r;
+int h;
+int k;
 
 typedef enum {
     I = 1, 
@@ -15,6 +15,10 @@ typedef enum {
     IV
 } quadrantType;
 
+bool process_args(int argc, char *argv[]);
+int process_arg(char* arg, int argPosition);
+int convert_arg(char* arg, int defaultValue);
+void apply_defaults();
 bool output_quadrant(quadrantType q);
 double get_x(int theta);
 double get_y(int theta);
@@ -25,15 +29,22 @@ double shift_y_for_center(double y);
 bool is_in_domain(int theta);
 bool is_in_range(double f);
 
-int main()
+int main(int argc, char *argv[])
 {
-    for (int i = I; i <= IV; i++)
-    {
-        int result = output_quadrant((quadrantType)i);
+    bool result = false;
 
-        if (!result)
+    result = process_args(argc, argv);
+
+    if (result)
+    {
+        for (int i = I; i <= IV; i++)
         {
-            exit(EXIT_FAILURE);
+            result = output_quadrant((quadrantType)i);
+
+            if (!result)
+            {
+                exit(EXIT_FAILURE);
+            }
         }
     }
     exit(EXIT_SUCCESS);
@@ -76,7 +87,7 @@ double get_x(int theta)
 
     sinResultInDegrees = sin(theta * (M_PI / 180.0));
 
-    value = sinResultInDegrees * (double)R;
+    value = sinResultInDegrees * (double)r;
     
     return value;
 }
@@ -90,7 +101,7 @@ double get_y(int theta)
 
     cosResultInDegrees = cos(theta * (M_PI / 180.0));
 
-    value = cosResultInDegrees * (double)R;
+    value = cosResultInDegrees * (double)r;
 
     return value;
 }
@@ -132,14 +143,14 @@ double transform_y_for_quadrant(double y, quadrantType q)
 double shift_x_for_center(double x)
 {
     double value = 0;
-    value = x - H;
+    value = x - h;
     return value;
 }
 
 double shift_y_for_center(double y)
 {
     double value = 0;
-    value = y - K;
+    value = y - k;
     return value;
 }
 
@@ -154,7 +165,73 @@ bool is_in_domain(int theta)
 bool is_in_range(double item)
 {
     bool value;
-    value = (item >= 0 && item <= (double)R);
+    value = (item >= 0 && item <= (double)r);
     return value;
+}
+
+bool process_args(int argc, char *argv[])
+{
+    apply_defaults();
+    
+    if (argc > 0)
+    {
+        if (strcmp(argv[1], "?") == 0 ||
+            strcmp(argv[1], "-h") == 0 ||
+            strcmp(argv[1], "--help") == 0)
+        {
+            printf("%s [r] [h] [k]\n", argv[0]);
+            printf("That's radius then starting coordinates\n");
+            return false;
+        }
+        if (argc > 1)
+        {
+            r = process_arg(argv[1], 1);
+            if (argc > 2)
+            {
+                h = process_arg(argv[2], 2);
+                if (argc > 3)
+                {
+                    k = process_arg(argv[3], 3);
+                }
+            }
+        }
+    }
+    return true;
+}
+
+int process_arg(char* arg, int argPosition)
+{
+    int value = 0;
+    switch (argPosition)
+    {
+        case 1:
+            value = convert_arg(arg, r);
+            break;
+        case 2:
+            value = convert_arg(arg, h);
+            break;
+        case 3:
+            value = convert_arg(arg, k);
+            break;
+    }
+    return value;
+}
+
+int convert_arg(char* arg, int defaultValue)
+{
+    int value;
+    value = atoi(arg);
+    if (value == 0)
+    {
+        value = defaultValue;
+    }
+    return value;
+}
+
+void apply_defaults()
+{
+    r = 5;
+    h = 0;
+    k = 0;
 }
 
